84d1c55c8943f4fc60cfcbfb9a68b5d1883d522b
luc34fenos <fenos.rasolondraibe@gmail.com>
2019-07-12 14:20:56 +0300

    -v1.0.7

--- app/models/web.rb
+++ app/models/web.rb
@@ -1,3 +1,4 @@
 class Web < ApplicationRecord
 	has_one :cv
+  has_one :company
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+f = User.new(username: "fenos", email: "fef@fef.fef", password: "34froot98", password_confirmation: "34froot98", status: "admin")
+f.save!
