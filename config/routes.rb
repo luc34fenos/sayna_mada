bed684a9d2a51a38113b8c3f7329f4f4455693a0
luc34fenos <fenos.rasolondraibe@gmail.com>
2019-07-18 18:44:38 +0300

    -v1.1.2

--- config/routes.rb
+++ config/routes.rb
@@ -36,4 +36,6 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :states, only: :index
+
+  match '*path' => 'home#not_found', via: :all
 end
