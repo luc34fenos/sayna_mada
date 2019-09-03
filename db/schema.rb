c759433745bd60d128e875ba923f1a68736c64c5
PlinXXX <plinsy2@gmail.com>
2019-09-03 12:14:51 +0300

    icon sayna

--- db/schema.rb
+++ db/schema.rb
@@ -68,7 +68,7 @@ ActiveRecord::Schema.define(version: 2019_08_03_064149) do
 
   create_table "cover_letters", force: :cascade do |t|
     t.bigint "cv_id"
-    t.text "content"
+    t.text "content", default: "<p align=\"left\" style=\"border: 0px; padding: 0px; margin-right: 0px; margin-bottom: 1.5em; margin-left: 0px; text-align: justify; color: rgb(51, 51, 51);\"><span class=\"txt-toreplace\" style=\"border: 0px; margin: 0px; padding: 0px; color: rgb(3, 0, 198);\">[Ton prénom et nom]</span><br><span class=\"txt-toreplace\" style=\"border: 0px; margin: 0px; padding: 0px; color: rgb(3, 0, 198);\">[Ton adresse]</span><br><span class=\"txt-toreplace\" style=\"border: 0px; margin: 0px; padding: 0px; color: rgb(3, 0, 198);\">[Ton téléphone]</span><br><span class=\"txt-toreplace\" style=\"border: 0px; margin: 0px; padding: 0px; color: rgb(3, 0, 198);\">[Ton email]</span></p><br style=\"color: rgb(51, 51, 51);\"><p align=\"right\" style=\"border: 0px; padding: 0px; margin-right: 0px; margin-bottom: 1.5em; margin-left: 0px; text-align: justify; color: rgb(51, 51, 51);\"><span class=\"txt-toreplace\" style=\"border: 0px; margin: 0px; padding: 0px; color: rgb(3, 0, 198);\">[Nom de l'entreprise]</span><br><span class=\"txt-toreplace\" style=\"border: 0px; margin: 0px; padding: 0px; color: rgb(3, 0, 198);\">[Nom/prénom/poste du destinataire]</span><br><span class=\"txt-toreplace\" style=\"border: 0px; margin: 0px; padding: 0px; color: rgb(3, 0, 198);\">[Adresse de l'entreprise]</span>&nbsp;<br></p><p align=\"right\" style=\"border: 0px; padding: 0px; margin-right: 0px; margin-bottom: 1.5em; margin-left: 0px; text-align: justify; color: rgb(51, 51, 51);\"><span class=\"txt-toreplace\" style=\"border: 0px; margin: 0px; padding: 0px; color: rgb(3, 0, 198);\">[Ville]</span>, le&nbsp;<span class=\"txt-toreplace\" style=\"border: 0px; margin: 0px; padding: 0px; color: rgb(3, 0, 198);\">[date]</span></p><br style=\"color: rgb(51, 51, 51);\"><p class=\"weight-bold\" style=\"border: 0px; padding: 0px; margin-right: 0px; margin-bottom: 1.5em; margin-left: 0px; text-align: justify; color: rgb(51, 51, 51);\"><b>Objet</b><span style=\"font-weight: 700;\"> : Candidature au poste de&nbsp;<span class=\"txt-toreplace\" style=\"border: 0px; margin: 0px; padding: 0px; color: rgb(3, 0, 198);\">[nom du poste auquel tu postules]</span></span></p><br style=\"color: rgb(51, 51, 51);\"><p style=\"border: 0px; padding: 0px; margin-right: 0px; margin-bottom: 1.5em; margin-left: 0px; text-align: justify; color: rgb(51, 51, 51);\">Madame, Monsieur,</p><p style=\"border: 0px; padding: 0px; margin-right: 0px; margin-bottom: 1.5em; margin-left: 0px; text-align: justify; color: rgb(51, 51, 51);\">Par le biais de la présente et du CV ci-joint, je souhaite vous soumettre mon profil et vous exprimer toute ma motivation pour exercer les fonctions de&nbsp;<span class=\"txt-toreplace\" style=\"border: 0px; margin: 0px; padding: 0px; color: rgb(3, 0, 198);\">[emploi pour lequel tu postules]</span>&nbsp;au sein de votre service de&nbsp;<span class=\"txt-toreplace\" style=\"border: 0px; margin: 0px; padding: 0px; color: rgb(3, 0, 198);\">[service pour lequel tu postules]</span>.</p><p style=\"border: 0px; padding: 0px; margin-right: 0px; margin-bottom: 1.5em; margin-left: 0px; text-align: justify; color: rgb(51, 51, 51);\">Lors de mes dernières expériences professionnelles, j’ai travaillé pour différentes entreprises du secteur&nbsp;<span class=\"txt-toreplace\" style=\"border: 0px; margin: 0px; padding: 0px; color: rgb(3, 0, 198);\">[secteur ou secteurs dans lesquels tu as travaillé]</span>, où j’ai pu acquérir et développer de solides connaissances en&nbsp;<span class=\"txt-toreplace\" style=\"border: 0px; margin: 0px; padding: 0px; color: rgb(3, 0, 198);\">[mentionne les programmes ou les domaines dans lesquels tu as acquis des connaissances]</span>, entre autres, que j’aimerais partager avec vous et continuer à approfondir au sein d’une équipe dynamique et stimulante.</p><p style=\"border: 0px; padding: 0px; margin-right: 0px; margin-bottom: 1.5em; margin-left: 0px; text-align: justify; color: rgb(51, 51, 51);\">Au cours de ma dernière expérience professionnelle à/chez&nbsp;<span class=\"txt-toreplace\" style=\"border: 0px; margin: 0px; padding: 0px; color: rgb(3, 0, 198);\">[dernière entreprise dans laquelle tu as travaillé]</span>, j’ai également eu l’occasion de contribuer au développement de différents projets, raison pour laquelle le fait de continuer à travailler au lancement de nouvelles idées et d’apprendre aux côtés d’une nouvelle équipe me motive énormément.</p><p style=\"border: 0px; padding: 0px; margin-right: 0px; margin-bottom: 1.5em; margin-left: 0px; text-align: justify; color: rgb(51, 51, 51);\">Je serai ravi(e) de vous rencontrer à l’avenir pour pouvoir parler de votre entreprise et de la mission proposée.</p><p style=\"border: 0px; padding: 0px; margin-right: 0px; margin-bottom: 1.5em; margin-left: 0px; text-align: justify; color: rgb(51, 51, 51);\">En vous remerciant pour le temps que vous aurez bien voulu accorder à ma lettre, je vous prie de bien vouloir agréer, madame, monsieur, mes plus cordiales salutations.</p><p style=\"border: 0px; padding: 0px; margin-right: 0px; margin-bottom: 1.5em; margin-left: 0px; text-align: justify; color: rgb(51, 51, 51);\"><span class=\"txt-toreplace\" style=\"border: 0px; margin: 0px; padding: 0px; color: rgb(3, 0, 198);\">[Ton prénom et nom]</span></p><p style=\"border: 0px; padding: 0px; margin-right: 0px; margin-bottom: 1.5em; margin-left: 0px; text-align: justify; color: rgb(51, 51, 51);\"><span class=\"txt-toreplace\" style=\"border: 0px; margin: 0px; padding: 0px; color: rgb(3, 0, 198);\">[Ta signature]</span></p>"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["cv_id"], name: "index_cover_letters_on_cv_id"
@@ -190,13 +190,14 @@ ActiveRecord::Schema.define(version: 2019_08_03_064149) do
     t.bigint "user_id"
     t.string "firstname"
     t.string "lastname"
-    t.string "sexe", default: "Non confirmé"
-    t.string "marital_status", default: "Compliqué"
+    t.string "sexe", default: "non spécifié"
+    t.string "marital_status", default: "compliqué"
     t.datetime "birthdate"
     t.string "tel"
     t.string "address"
     t.bigint "city_id"
     t.boolean "is_activated?", default: false
+    t.boolean "is_free?", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["city_id"], name: "index_students_on_city_id"
@@ -232,12 +233,12 @@ ActiveRecord::Schema.define(version: 2019_08_03_064149) do
   end
 
   create_table "webs", force: :cascade do |t|
-    t.bigint "cv_id"
     t.bigint "company_id"
     t.string "name"
     t.string "link"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_webs_on_company_id"
   end
 
   add_foreign_key "backgrounds", "cvs"
@@ -247,4 +248,5 @@ ActiveRecord::Schema.define(version: 2019_08_03_064149) do
   add_foreign_key "posts", "companies"
   add_foreign_key "staffs", "companies"
   add_foreign_key "swebs", "cvs"
+  add_foreign_key "webs", "companies"
 end
